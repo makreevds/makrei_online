6425060ec4a27dda70ebf105240c4bf1e1eba7f2
makreevds <makreevds@yandex.ru>
2025-11-15 00:33:55 +0300

    NEW_SITEEEEEEEEEEE

--- core/urls.py
+++ core/urls.py
@@ -3,40 +3,10 @@ URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from main.views import index, register, login_view, logout_view
-from blog.views import post_list, post_create, post_edit, post_delete, user_search, user_posts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('register/', register, name='register'),
-    path('blog/', post_list, name='blog'),
-    path('blog/create/', post_create, name='post_create'),
-    path('blog/<int:post_id>/edit/', post_edit, name='post_edit'),
-    path('blog/<int:post_id>/delete/', post_delete, name='post_delete'),
-    path('users/search/', user_search, name='user_search'),
-    path('users/<str:username>/', user_posts, name='user_posts'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
 ]
-
-# Обработка статических файлов для разработки
-if settings.DEBUG:
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
